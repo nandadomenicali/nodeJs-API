249fdea3f99f7f0079e16c1882b72a3e75cc73d0
Fernanda Domenicali <nanda21domenicali@gmail.com>
2021-07-07 22:06:14 -0300

    query for database connection

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,11 +1,9 @@
 module.exports = app => {
-    app.get('/atendimentos', (req, res) => res.send('Você está na rota de atendimentos e está realizando um GET'))
+    app.get('/atendimentos', (req, res) => res.send('Servidor rodando'))
 
-    app.post('/atendimentos', (req, res) => {
-        console.log('Atendimento enviado')
-        console.log(req.body)
-        res.send('Post atendimento')
-    })
-     
-     
+    app.post('./atendimentos', (req, res) => {
+        
+        res.send('POST')
+})
+
 }
