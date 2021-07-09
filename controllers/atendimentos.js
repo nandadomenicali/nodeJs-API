a9c0116f015fd85945e8fcdacb6c385650975735
Fernanda Domenicali <nanda21domenicali@gmail.com>
2021-07-09 09:42:29 -0300

    include http status code

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,9 +1,12 @@
+const Atendimento = require('../models/atendimento')
+
 module.exports = app => {
     app.get('/atendimentos', (req, res) => res.send('Servidor rodando'))
 
     app.post('./atendimentos', (req, res) => {
+        const atendimento = req.body
         
-        res.send('POST')
+        Atendimento.adiciona(atendimento, res)
 })
 
 }
