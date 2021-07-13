242015272b5b26074f57cae8059d34a40920f8d9
Fernanda Domenicali <nanda21domenicali@gmail.com>
2021-07-13 17:54:59 -0300

    Axios to consume an API

--- models/atendimento.js
+++ models/atendimento.js
@@ -1,4 +1,6 @@
+const { default: axios } = require('axios')
 const moment = require('moment')
+const axios = require('axios')
 const conexao = require ('../infraestrutura/conexao.js')
 
 class Atendimento{
@@ -60,11 +62,16 @@ class Atendimento{
     buscaPorId(id, res){
         const sql = 'SELECT * FROM Atendimentos WHERE id=${id}'
 
-        conexao.query(sql, (erro, resultados) => {          
+        conexao.query(sql, async (erro, resultados) => {          
             const atendimento = resultados[0]
+            const cpf = atendimento.cliente
             if(erro){
                 res.status(400).json(erro)
             }else{
+                const { data } = await axios.get(`http://localhost:8082/${cpf}`)
+
+                atendimento.cliente = data
+
                 res.status(200).json(atendimento)    
             }                                                                           
         })                                                               
