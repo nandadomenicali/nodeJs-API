d51f9c200ad727be361cad9b45608c9049343de1
Fernanda Domenicali <nanda21domenicali@gmail.com>
2021-07-14 19:54:13 -0300

    refactor miss

--- repositorio/Atendimento.js
+++ repositorio/Atendimento.js
@@ -2,8 +2,8 @@ const query = require('../infraestrutura/database/queries')
 
 class Atendimento{
     adiciona(atendimento) {
-        const sql  = 'INSERT INTO Atendimentos SET ?' 
-        query()
+        const sql  = 'INSERT INTO Atendimentos SET ?'  
+        return query(sql, atendimento)
     }
 
 }
